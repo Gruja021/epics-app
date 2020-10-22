8e03522b1fb06b77e0410bd566f7e482cfeda3d7
Desko Sekulovic <desko@Deskos-MacBook-Pro.local>
2020-10-22 15:40:41 +0200

    add data to reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
+import { State } from "./reducers/mainReducer";
+
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
-  return <div className="App">Epics App</div>;
+  const error = useSelector((state: State) => state.mainReducer.error);
+  const confirmed = useSelector((state: State) => state.mainReducer.confirmed);
+  const deaths = useSelector((state: State) => state.mainReducer.deaths);
+  const temp = useSelector((state: State) => state.mainReducer.temp);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: "TOTAL" });
+  }, [dispatch]);
+
+  return (
+    <>
+      {/* { This is just to see if everything's ok. Will be changed! } */}
+      <div>{error && error}</div>
+      <div>{confirmed && confirmed}</div>
+      <div>{deaths && deaths}</div>
+      <div>temp: {temp}</div>
+      <button
+        onClick={() => dispatch({ type: "TOTAL_COUNTRY", payload: "Serbia" })}
+      >
+        Serbia
+      </button>
+      <button
+        onClick={() => dispatch({ type: "TOTAL_COUNTRY", payload: "Denmark" })}
+      >
+        Denmark
+      </button>
+      <button
+        onClick={() => dispatch({ type: "TOTAL_COUNTRY", payload: "France" })}
+      >
+        France
+      </button>
+    </>
+  );
 }
 
 export default App;
--- src/reducers/mainReducer.ts
+++ src/reducers/mainReducer.ts
@@ -1,11 +1,37 @@
-const initialState = {
-  totalInfected: 0,
+interface Data {
+  confirmed: number | null;
+  deaths: number | null;
+  error: string | null;
+  temp: number | undefined;
+}
+
+export interface State {
+  mainReducer: Data;
+}
+
+const initialState: Data = {
+  confirmed: null,
+  deaths: null,
+  error: null,
+  temp: undefined,
 };
 
 const mainReducer = (
   state = initialState,
   action: { type: string; payload: any }
 ) => {
+  switch (action.type) {
+    case "COUNTRY":
+      return {
+        ...state,
+        confirmed: action.payload.confirmed,
+        deaths: action.payload.deaths,
+      };
+    case "ERROR":
+      return { ...state, error: action.payload };
+    case "GET_WEATHER":
+      return { ...state, temp: action.payload };
+  }
   return state;
 };
 
